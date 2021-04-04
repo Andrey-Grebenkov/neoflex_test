701cbec00057a906330a49d06c6da91ff6f3f856
Andrew <andrewmen96@gmail.com>
2021-04-04 22:53:56 +0400

    Created project

---
+++ test-project/src/test/java/com/andrewmen96/JsbinPage.java
@@ -0,0 +1,38 @@
+package com.andrewmen96;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class JsbinPage {
+
+    public static int i;
+
+    public WebDriver driver;
+
+    public JsbinPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+
+    @FindBy(xpath = "//*[text()='(X) Закрыть счет']")
+    private List<WebElement> closePriceButton;
+
+    @FindBy(xpath = "//*[contains(@class,'acc-')]//b")
+    private List<WebElement> balance;
+
+    public void delete() {
+//        List<WebElement> closePriceButton = driver.findElements(By.xpath("//*[text()='(X) Закрыть счет']"));
+//        List<WebElement> balance = driver.findElements(By.xpath("//*[contains(@class,'acc-')]//b"));
+        for (WebElement ba : balance) {
+            if (ba.getText().equals("0"))
+                if (balance.get(i).getText().equals("0"))
+                    closePriceButton.get(i).click();
+        }
+    }
+}
---
+++ test-project/src/test/java/com/andrewmen96/Tests.java
@@ -0,0 +1,37 @@
+package com.andrewmen96;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Tests {
+    public static WebDriver driver;
+    public static JsbinPage jsbin;
+
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperies("chromedriver"));
+        driver = new ChromeDriver();
+        jsbin = new JsbinPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperies("jsbinpage"));
+    }
+
+    @Test
+    public void loginTest() {
+        driver.switchTo().frame("<proxy>");
+        driver.switchTo().frame("JS Bin Output ");
+        jsbin.delete();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
